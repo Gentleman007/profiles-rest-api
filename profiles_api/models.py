844b91a1e61ca74936e50526feca4080fc596c8f
jagadeesh <jj71116@gmail.com>
2020-06-22 23:47:47 +0530

    enbaled django admin for userprofiles model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -4,29 +4,30 @@ from django.contrib.auth.models import PermissionsMixin,BaseUserManager
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
 
-    def create_user(self,email,name, password=None):
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
         if not email:
-            raise ValueError("user must have email address")
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
-        user = self.model(email = email ,name =name)
+        user = self.model(email=email, name=name,)
+
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
-    def create_superuser(self,email,name,password):
-        user  = self.create_user(email,name,password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-        user.is_superuser =True
+        user.is_superuser = True
         user.is_staff = True
-
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
-
-
-
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
@@ -39,7 +40,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         return self.name
